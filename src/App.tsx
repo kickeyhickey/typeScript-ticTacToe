e66abadb1f5a7f4d2c39cec9634b255c3046f683
kickeyhickey <hickeydeveloper@gmail.com>
2022-09-10 23:01:58 -0700

    initial push

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component<{}, {board: any}> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      board: Array(9).fill("X")
+    }
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <h1>Tic Tac Toe</h1>
+        <div className='board'>
+          {this.state.board.map((box:any) => {
+            return (
+              <div className='box'>{box}</div>
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
